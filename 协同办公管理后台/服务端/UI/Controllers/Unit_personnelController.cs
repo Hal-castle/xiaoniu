bd95b77ce3b78e9139fb2dfa5d9f3b1ad0b7b538
lifeixian <1964066365@qq.com>
2021-02-25 16:25:21 +0800

    控制器修改补充2

--- 协同办公管理后台/服务端/UI/Controllers/Unit_personnelController.cs
+++ 协同办公管理后台/服务端/UI/Controllers/Unit_personnelController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SqlSugar;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,9 +8,19 @@ using System.Threading.Tasks;
 
 namespace UI.Controllers
 {
+    [Route("api/[controller]/[action]")]
+    [ApiController]
     //单位人事
     public class Unit_personnelController : Controller
     {
+        private UnitOfWork ff = null;//SqlSugar框架的实体类
+        private readonly ILogger<ValuesController> _logger;
+        //构造函数
+        public Unit_personnelController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+            ff = new UnitOfWork();
+        }
         #region 单位管理
         public IActionResult Index()
         {
